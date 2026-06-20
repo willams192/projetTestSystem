6bee2f8bb0c113f1be1072d5e03d676d9edf1b13
DiegoFeBrandao <diegoferreirab26@gmail.com>
2026-06-20 12:14:16 -0300

    criação do conftest e substitui setup/teardown por fixture do conftest no test1

---
+++ tests/conftest.py
@@ -0,0 +1,18 @@
+import pytest
+from selenium import webdriver
+
+from pages.LoginPage import LoginPage
+
+
+@pytest.fixture
+def open_browser():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.implicitly_wait(10)
+
+    login_page = LoginPage(driver)
+    login_page.open_login_page()
+
+    yield login_page
+
+    login_page.close()
--- tests/test1.py
+++ tests/test1.py
@@ -1,17 +1,8 @@
-from selenium import webdriver
 from pages.LoginPage import LoginPage
 
 
 class TestLoginPage:
-    def setup_method(self):
-        self.driver = webdriver.Chrome()
-        self.driver.maximize_window()
-        self.driver.implicitly_wait(10)
 
-    def teardown_method(self):
-        self.driver.quit()
-
-    def test_login_page(self):
-        login_page = LoginPage(self.driver)
-        login_page.open_login_page()
+    def test_login_page(self, open_browser):
+        login_page = open_browser
         login_page.click_login_customer_button()
